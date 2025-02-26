8d3406f6f1b5fb15ff2975caa161a5e73b4d5afc
vinay23is <55172995+vinay23is@users.noreply.github.com>
2025-02-26 13:40:37 -0600

    P18

---
+++ P18_Alpha_Triangle.java
@@ -0,0 +1,24 @@
+package myDSA;
+public class P18_Alpha_Triangle {
+    public static void main(String arg[])
+    {
+    int N=4;
+    for(int i=0;i<N;i++){
+          
+        // Inner loop for printing the alphabets from
+        // A + N -1 -i (i is row no.) to A + N -1 ( E in this case).
+        for(char ch =(char)(int)('A'+N-1-i);ch<=(char)(int)('A'+N-1);ch++){
+            
+            System.out.print(ch + " ");
+        }
+        
+        // As soon as the letters for each iteration are printed, we move to the
+        // next row and give a line break otherwise all letters
+        // would get printed in 1 line.
+        System.out.println();
+    }
+    
+    }
+}
+    
+
